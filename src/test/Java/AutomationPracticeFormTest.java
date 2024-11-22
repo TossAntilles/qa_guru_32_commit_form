5da202c3e80e2b1a50c988f9fc6358e53b39f3f5
Anton Bogutskii <schicksalkreuzung@gmail.com>
2024-11-22 04:55:39 +0300

    Added validation of submit results

--- src/test/Java/AutomationPracticeFormTest.java
+++ src/test/Java/AutomationPracticeFormTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTest {
@@ -18,6 +19,9 @@ public class AutomationPracticeFormTest {
     @Test
     void succesfullFormFill(){
         open("/automation-practice-form");
+
+        //Form input
+
         $("#userForm #firstName").setValue("John");
         $("#userForm #lastName").setValue("Doe");
         $("#userForm #userEmail").setValue("anonymous@anonymous.com");
@@ -33,7 +37,7 @@ public class AutomationPracticeFormTest {
         $(".react-datepicker__year-select").selectOptionByValue("1900");
         $(".react-datepicker__day.react-datepicker__day--001").click();
 
-        //subjects
+        //subjects - validation of multiple choise
         $("#userForm #subjectsInput").setValue("Maths");
         $("#userForm .subjects-auto-complete__menu").click();
         $("#userForm #subjectsInput").setValue("Arts");
@@ -41,7 +45,6 @@ public class AutomationPracticeFormTest {
 
         //Checkbox
         //$("#userForm #hobbies-checkbox-1").setSelected(true);  //Cannot change invisible element?
-        //$("#userForm #hobbies-checkbox-3").setSelected(true);
         $("#userForm [for=hobbies-checkbox-1]").click();
         $("#userForm [for=hobbies-checkbox-3]").click();
 
@@ -50,11 +53,27 @@ public class AutomationPracticeFormTest {
 
         //Address Lists
         $("#userForm #currentAddress").setValue("Random Street, 1138");
+        //enter instead of selection from list - could not find IDs
         $("#userForm #react-select-3-input").setValue("NCR").pressEnter();
         $("#userForm #react-select-4-input").setValue("Delhi").pressEnter();
 
-        sleep(10000);
 
+        $("#userForm #submit").click();
+
+        // Validating results
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table").shouldHave(text("Student Name")).shouldHave(text("John Doe"));
+        $(".table").shouldHave(text("Student Email")).shouldHave(text("anonymous@anonymous.com"));
+        $(".table").shouldHave(text("Gender")).shouldHave(text("Other"));
+        $(".table").shouldHave(text("Mobile")).shouldHave(text("1234567890"));
+        $(".table").shouldHave(text("Date of Birth")).shouldHave(text("01 January,1900"));
+        $(".table").shouldHave(text("Subjects")).shouldHave(text("Maths, Arts"));
+        $(".table").shouldHave(text("Hobbies")).shouldHave(text("Sports, Music"));
+        $(".table").shouldHave(text("Picture")).shouldHave(text("image.jpg"));
+        $(".table").shouldHave(text("Address")).shouldHave(text("Random Street, 1138"));
+        $(".table").shouldHave(text("State and City")).shouldHave(text("NCR Delhi"));
 
     }
 }
